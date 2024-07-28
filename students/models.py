95806c3136e5fa92b0314df3060651c56e23782b
Aman <amansingh945420@gmail.com>
2024-07-28 18:45:15 +0530

    Initial commit

---
+++ students/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Students(models.Model):
+    student_number=models.PositiveIntegerField()
+    first_name=models.CharField(max_length=60)
+    last_name=models.CharField(max_length=60)
+    email=models.EmailField(max_length=100)
+    field_of_study=models.CharField(max_length=50)
+    gpa=models.FloatField()
+
+    def __str__(self):
+        return f'Student: {self.first_name} {self.last_name}'
+    
+    
+    
