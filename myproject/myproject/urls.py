8ab95b5dd8e9d1ec93658b30221d968e63c20d2f
Aman <amansingh945420@gmail.com>
2024-11-26 16:59:27 +0530

    Added logout functionality and updated base.html

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for django_project project.
+URL configuration for myproject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,18 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from students.views import login_view  # Import login_view explicitly
-
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # Admin interface
-    path('', login_view, name='login'),  # Root URL for login page
-    path('students/', include('students.urls')),  # Include student app URLs
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # JWT Token API
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # Token refresh API
+    path('admin/', admin.site.urls),
 ]
--- students/models.py
+++ students/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class Students(models.Model):
     student_number=models.PositiveIntegerField()
@@ -13,4 +13,20 @@ class Students(models.Model):
         return f'Student: {self.first_name} {self.last_name}'
     
     
-    
+class User(AbstractUser):
+    ROLE_CHOICES = [
+        ('admin', 'Admin'),
+        ('teacher', 'Teacher'),
+        ('student', 'Student'),
+    ]
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='student')
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='students_user_set',  # Add a unique related_name
+        blank=True,
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='students_user_set',  # Add a unique related_name
+        blank=True,
+    )
