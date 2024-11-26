940b8a3a7c8e92e5e902b51913e9a80cd8a54de1
Aman <amansingh945420@gmail.com>
2024-11-26 17:08:30 +0530

    Added functionality

--- django_project/settings.py
+++ django_project/settings.py
@@ -62,8 +62,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = '29770csaiml@gmail.com'  # Replace with your Gmail address
-EMAIL_HOST_PASSWORD = 'ASDFGHjkl@12'  # Replace with your Gmail password or app password
+EMAIL_HOST_USER = 'Replace with your Gmail address'  # Replace with your Gmail address
+EMAIL_HOST_PASSWORD = 'Replace with your Gmail password or app password'  # Replace with your Gmail password or app password
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 TEMPLATES = [
     {
